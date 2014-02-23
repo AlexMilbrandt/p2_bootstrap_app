78d7d2c339db9f260989aae07fb717e31fe2595c
Alex Milbrandt <amilbrandt@wisc.edu>
2014-02-23 12:16:18 -0600

    Initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+P2BootstrapApp::Application.config.secret_key_base = '499d44b4b8bda7d1cb5f0c5790c26e971a6f363ff02085fe7a2a73f67b2abe0b439cf57f6feb844fac6809a89612cea6cbadbce2a6d2ac7e54833c08ad19c1df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+P2BootstrapApp::Application.config.session_store :cookie_store, key: '_p2_bootstrap_app_session'
